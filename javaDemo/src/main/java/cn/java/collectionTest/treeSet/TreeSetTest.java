2ea1e7db3c8988bc80101482c36d0f0aca40c844
EricChang82 <changlejc@Hotmail.com>
2019-04-11 09:50:00 +0800

    1

---
+++ javaDemo/src/main/java/cn/java/collectionTest/treeSet/TreeSetTest.java
@@ -0,0 +1,27 @@
+package cn.java.collectionTest.treeSet;
+
+import java.util.TreeSet;
+
+public class TreeSetTest implements Comparable<Object>{
+     public static void main(String[] args) {
+         TreeSet<Object> treeSet1 = new TreeSet<Object>();
+         treeSet1.add("aa");
+         treeSet1.add("aa");
+         treeSet1.add(new TreeSetTest());
+         treeSet1.add(new TreeSetTest());
+         
+         for (Object obj : treeSet1) {
+            System.out.println(obj);
+        }
+         
+         
+     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(Object o) {
+        System.out.println("in compareTo:"+o.toString());
+        return 1;
+    }
+}
